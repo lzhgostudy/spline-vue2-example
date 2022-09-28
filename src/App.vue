73f644d5de53a322308ebf2011a83037db9cc6bd
Zhihua.Lu <623798099@qq.com>
2022-09-28 16:46:47 +0800

    spline example

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <Spline :scene="scene" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Spline from "spline-vue/v2";
 
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  components: { Spline },
+  data() {
+    return {
+      scene: "https://prod.spline.design/6Wq1Q7YGyM-iab9i/scene.splinecode",
+    };
   },
 };
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  height: 100vh;
 }
 </style>
